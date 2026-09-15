2b9b3e88fa16c2aaeead615b616cc5e1abf1839a
Emanuele <egindustries11@gmail.com>
2026-09-15 17:16:53 +0200

    Restore working SuperBLT metadata and dispatcher defaults.
    #Fix
    - Restore the known-working mod.txt color and blt_version formats and bump version to 1.5.1.
    #Cleanup
    - Restore PD25.settings placeholders in mod.lua to match the working dispatcher entry point.

--- mod.lua
+++ mod.lua
@@ -1,21 +1,28 @@
--- Entry point for every SuperBLT post-hook. supermod.xml always runs this file;
--- we then load lua/<scriptname>.lua matching the hooked game script (RequiredScript).
 if not PD25 then
 	PD25 = {}
 	PD25.required = {}
 	PD25.mod_path = ModPath
 	PD25.mod_instance = ModInstance
 	PD25.version = "19"
-	PD25.settings = {}
+	
+	-- You can change these settings if you want
+	-- Most of these currently bring some Streamlined Heisting tweaks to this mod if you didn't had SH installed
+	-- In the future, i might add some customization utilizing this.
+	PD25.settings = {
+		skirmish_tweaks = true,
+		so_tweaks = true,
+		sniper_tweaks = true,
+		shield_tweaks = true,
+	}
 end
 
--- Load each hooked script once (e.g. lib/.../playermaskoff -> lua/playermaskoff.lua).
 if RequiredScript and not PD25.required[RequiredScript] then
-	local fname = PD25.mod_path .. RequiredScript:gsub(".+/(.+)", "lua/%1.lua")
 
+	local fname = PD25.mod_path .. RequiredScript:gsub(".+/(.+)", "lua/%1.lua")
 	if io.file_is_readable(fname) then
 		dofile(fname)
 	end
 
 	PD25.required[RequiredScript] = true
+
 end
